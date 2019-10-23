f180e8332cf59ed5203f8c7e8eaaf0ad7a6d2fc8
Rizkiansyah <rixkiansyah@gmail.com>
2019-10-23 22:31:15 +0700

    update millify indo

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# millify
+# millify-indo
 Convert long numbers into a human-readable format, e.g. `25000` to `'25Rb'`
 
-This script owned by **Izolate**, just forked to Indonesian common Postfixes.
+This script originally created by **Izolate**, Customized by **Workaja** using Indonesian common Postfixes.
+
+### Supported Sizing
+
+- Money (we call it: *uang* and *duit*)
+- Weight (we use basic weight on: *gram/g* and *miligram/mg*)
+- Length (we use basic length on: *milimeter/mm, centimeter/cm* and *meter/m* )
+- Storage (we call it: *storage, harddisk, penyimpanan* and *hdd*)
 
 ### Installation
 ```bash
@@ -9,20 +16,22 @@ npm i millify-indo
 ```
 
 ### Usage
-#### `millify(@number, @decimal)`
+#### `millify-indo(@angka, @tingkat, @satuan)`
 
-First parameter is the number, second parameter is the desired decimal places (default: 1).
+- First parameter is the number
+
+- Second parameter is the type like **uang, duit, gram, penyimpanan** or else (look above).
 
 ```js
-import millify from 'millify'
+import millify from 'millify-indo'
 
-millify(2500)
-// 2.5Rb
+millify(2500, 3, 'uang')
+// 2.5Ribu
 
-millify(1250000, 3)
-// 1.25Jt
+millify(1250000, 6, 'hdd')
+// 1.25Megabyte
 
 millify(2000000000)
-// 2M
+// 2Milyar
 ```
 
--- index.js
+++ index.js
@@ -1,23 +1,72 @@
-const suffixes = new Map()
-suffixes.set(3, 'Rb')
-suffixes.set(6, 'Jt')
-suffixes.set(9, 'M')
-suffixes.set(12, 'T')
+const indonesia_uang = new Map([
+[3, 'Ribu'],[6, 'Juta'],[9, 'Milyar'],[12, 'Triliun'],[15, 'Dwiyar'],[18, 'Trita'],
+[21, 'Triyar'],[24, 'Caturta'],[27, 'Caturyar'],[30, 'Pancata'],[33, 'Pancayar'],
+[36, 'Sadta'],[39, 'Sadyar'],[42, 'Saptata'],[45, 'Saptayar'],[48, 'Hastata'],
+[51, 'Hastayar'],[54, 'Nawata'],[57, 'Nawayar'],[60, 'Dasata'],[63, 'Dasayar']
+]);
 
-module.exports = (ugly, decimal) => {
-  // Make sure value is a number
+const indonesia_miligram = new Map([
+  [2, 'Gram'],[3, 'Kilogram'],[4, 'Ton'],[5, 'Kiloton'],[6, 'Megaton']
+]);
+
+const indonesia_gram = new Map([
+  [2, 'Kilogram'],[3, 'Ton']
+]);
+
+const indonesia_milimeter = new Map([
+  [1, 'Centimeter'],[2, 'Dekameter'],[3, 'Meter'],[4, 'Desimeter'],[5, 'Hektometer'],
+  [6, 'Kilometer']
+]);
+
+const indonesia_centimeter = new Map([
+  [1, 'Dekameter'],[2, 'Meter'],[3, 'Desimeter'],[4, 'Hektometer'],[5, 'Kilometer']
+]);
+
+const indonesia_meter = new Map([
+  [1, 'Desimeter'],[2, 'Hektometer'],[3, 'Kilometer']
+]);
+
+const indonesia_hdd = new Map([
+  [3, 'Kilobyte'],[6, 'Megabyte'],[9, 'Gigabyte'],[12, 'Terrabyte'],[15, 'Petabyte']
+]);
+
+module.exports = (ugly, decimal, satuan = 'default') => {
   ugly = (num => {
-    if (typeof num !== 'number') throw new Error('Input value is not a number')
+    if (typeof num !== 'number') throw new Error('Masukan bukan angka')
     else return parseFloat(num)
   })(ugly)
 
-  // Figure out how many digits in the integer
   const digits = Math.floor(Math.log10(Math.abs(ugly))) + 1
 
-  // Figure out the appropriate unit for the number
+  let suf = indonesia_meter;
+
+  switch (satuan) {
+    case 'uang': case 'duit':
+      suf = indonesia_uang;
+      break;
+    case 'mg': case 'miligram':
+      suf = indonesia_miligram;
+      break;
+    case 'g': case 'gram':
+      suf = indonesia_gram;
+      break;
+    case 'mm': case 'milimeter':
+      suf = indonesia_milimeter;
+      break;
+    case 'cm': case 'centimeter':
+      suf = indonesia_centimeter;
+      break;
+    case 'harddisk': case 'storage': case 'penyimpanan': case 'hdd':
+      suf = indonesia_hdd;
+      break;
+    case 'm': case 'meter': case 'meteran': case 'default': default:
+      suf = indonesia_meter;
+      break;
+  }
+
   const units = ((num, zeroes) => {
-    for (let z of suffixes.keys()) if (num > z) zeroes = z
-    return { suffix: suffixes.get(zeroes), zeroes }
+    for (let z of suf.keys()) if (num > z) zeroes = z
+    return { suffix: suf.get(zeroes), zeroes }
   })(digits, null)
 
   const pretty = ugly/Math.pow(10, units.zeroes)
